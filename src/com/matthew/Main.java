12f9bc1a7f45cb51ca50b651a6d2d51cbcf9ac5c
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-25 12:48:16 -0600

    updated

--- src/com/matthew/Main.java
+++ src/com/matthew/Main.java
@@ -1,8 +1,25 @@
 package com.matthew;
-
+import java.awt.*;
+import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Hello World!");
+//        you can reassign these are all primitive types
+//        byte age = 30;
+//        when using long even though it is over 3 billion you have to specify with a L at the end of it
+//        long viewCount = 3_123_456_678L;
+//        it is the same with float
+//        float price = 10.99F;
+//        char is a single letter and str is several
+//       char letter = 'A';
+//        boolean isEligible = true;
+//        everything else is reference types they work with complex objects you have to allocate memory with reference types
+//        reference types when reassigned are not independent of each other
+//        Date now = new Date();
+//        System.out.println(now);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
