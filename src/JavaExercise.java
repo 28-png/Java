52f27d50f681fa164241025c42722898cd49f21b
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-21 09:16:02 -0600

    updated

--- src/JavaExercise.java
+++ src/JavaExercise.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
+
 public class JavaExercise {
 
     public static String compare(int a, int b) {
     if(a < b)
              return  a + " < " + b;
     else if(a > b)
-        return a + " > " + b;
+       return a + " > " + b;
     else
         return a + " == " + b;
 }
+
+public static ArrayList<ArrayList<Integer>> list(ArrayList<Integer> listToTry, int target) {
+        ArrayList<ArrayList<Integer>> sums = new ArrayList<>();
+        return sums;
+    }
 
     public static void main(String[] args) {
         System.out.println(compare(1, 2));
